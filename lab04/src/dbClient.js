5ec0bbc009e00b375a283c6fee41c2eab5b6d0b9
Patrawi27 <67262217+patrawi@users.noreply.github.com>
2022-10-11 19:22:12 +0700

    chore: fix db connection

--- lab04/src/dbClient.js
+++ lab04/src/dbClient.js
@@ -2,9 +2,11 @@ var redis = require("redis");
 const configure = require('./configure')
 
 const config = configure()
+console.log(process.env.REDIS_HOST)
+console.log(process.env.REDIS_PORT)
 var db = redis.createClient({
-  host: process.env.REDIS_HOST | config.redis.host,
-  port: process.env.REDIS_PORT | config.redis.port,
+  host: process.env.REDIS_HOST || config.redis.host,
+  port: process.env.REDIS_PORT || config.redis.port,
   retry_strategy: () => {
     return new Error("Retry time exhausted")
   }
