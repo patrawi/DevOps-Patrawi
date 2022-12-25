c13a143573e48a80145190755633d8d4e721c00e
Patrawi27 <67262217+patrawi@users.noreply.github.com>
2022-12-25 21:49:52 +0700

    fix: deployment

--- userapi/api/home.js
+++ userapi/api/home.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const homeRouter = express.Router();
 
-homeRouter.get('/', (req, res) => res.send('Hello World!'));
+homeRouter.get('/', (req, res) => res.send('Hello World! 123456789'));
 
 module.exports = homeRouter;
