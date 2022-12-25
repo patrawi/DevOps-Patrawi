a88e9bbb45b48feacd9388dcf72becb14c278855
Patrawi27 <67262217+patrawi@users.noreply.github.com>
2022-12-25 21:26:51 +0700

    feat: deploy

--- userapi/index.js
+++ userapi/index.js
@@ -23,7 +23,7 @@ app.use('/user', userRouter);
 app.use('/healthcheck', healthCheckRouter);
 const server = app.listen(port, (err) => {
   if (err) throw err;
-  console.log('Server listening the dai her dport ' + port);
+  console.log('Server listening the dai her dpdort ' + port);
 });
 
 module.exports = server;
